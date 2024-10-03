1a14304d03ddbe23da253fe5fea6072fe87b339a
Gil <gil.rossi.aguiar@live.com>
2024-10-03 13:16:36 -0300

    Tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpemsesApp());
@@ -18,8 +20,23 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // ignore: sized_box_for_whitespace
+          Container(
+            // ignore: prefer_const_constructors
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          )
+        ],
       ),
     );
   }
